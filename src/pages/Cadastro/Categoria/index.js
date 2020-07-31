5b195ec764c33d2f1d86d18f9afa1489a79a5a77
JONATHAN MARQUES DA COSTA <jonathanmarquesdacosta@gmail.com>
2020-07-31 11:31:39 -0300

    First commit

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -31,18 +31,18 @@ function CadastroCategoria() {
   // ============
 
   useEffect(() => {
-    if (window.location.href.includes('localhost')) {
-      const URL = 'http://localhost:8080/categorias';
-      fetch(URL)
-        .then(async (respostaDoServer) => {
-          if (respostaDoServer.ok) {
-            const resposta = await respostaDoServer.json();
-            setCategorias(resposta);
-            return;
-          }
-          throw new Error('Não foi possível pegar os dados');
-        });
-    }
+    // if (window.location.href.includes('localhost')) {
+    const URL = window.location.href.includes('localhost') ? 'http://localhost:8080/categorias' : 'https://jmflix.herokuapp.com/categorias';
+    fetch(URL)
+      .then(async (respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategorias(resposta);
+          return;
+        }
+        throw new Error('Não foi possível pegar os dados');
+      });
+    // }
   }, []);
 
   return (
